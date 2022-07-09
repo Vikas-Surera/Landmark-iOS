8d52e1f629fcbef96c4ea712f635c5c0294fe997
Vikas Surera <vikassurera@gmail.com>
2022-07-09 15:13:56 +0530

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Vikas Surera on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
